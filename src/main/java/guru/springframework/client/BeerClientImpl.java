3fe4069bdeb59c8a4ba62f48cd0d931ab0c108eb
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-05-15 15:52:27 +0100

    Uri Components Builder

--- src/main/java/guru/springframework/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/client/BeerClientImpl.java
@@ -6,6 +6,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Map;
 
@@ -13,6 +14,7 @@ import java.util.Map;
 public class BeerClientImpl implements BeerClient {
 
     public static final String BEER_PATH = "/api/v3/beer";
+    public static final String BEER_PATH_ID = "/api/v3/beer" + "/{beerId}";
     private final WebClient webClient;
 
     public BeerClientImpl(WebClient.Builder webClientBuilder) {
@@ -42,5 +44,14 @@ public class BeerClientImpl implements BeerClient {
     public Flux<BeerDTO> listBeerDtos() {
         return webClient.get().uri(BEER_PATH)
                 .retrieve().bodyToFlux(BeerDTO.class);
+    }
+
+    @Override
+    public Mono<BeerDTO> getBeerById(String id) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder.path(BEER_PATH_ID)
+                        .build(id))
+                .retrieve()
+                .bodyToMono(BeerDTO.class);
     }
 }
